8354010f7feabdec758accdda9eb9e219e671b89
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-10-31 00:27:02 -0700

    ready to deploy

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast, Zoom, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Cookies from "universal-cookie";
 import axios from "axios";
+import Particles from "react-tsparticles";
 
-import signinImage from "../assets/signup.jpg";
+const cookies = new Cookies();
 
 const initialState = {
   fullName: "",
@@ -14,17 +17,49 @@ const initialState = {
 };
 
 const Auth = () => {
+  useEffect(() => {
+    toast.info(
+      "Hey if you're new here, try logging in with username: guest, password: password",
+    );
+  }, []);
+
   const [form, setForm] = useState(initialState);
-  const [isSignup, setIsSignup] = useState(true);
+  const [isSignup, setIsSignup] = useState(false);
 
   const handleChange = e => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
-    console.log(form);
+    const { username, password, phoneNumber, avatarURL } = form;
+
+    const URL = "http://localhost:5000/auth";
+    // const URL = 'https://medical-pager.herokuapp.com/auth';
+
+    const {
+      data: { token, userId, hashedPassword, fullName },
+    } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
+      username,
+      password,
+      fullName: form.fullName,
+      phoneNumber,
+      avatarURL,
+    });
+
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("userId", userId);
+
+    if (isSignup) {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
+    }
+
+    window.location.reload();
   };
 
   const switchMode = () => {
@@ -35,12 +70,13 @@ const Auth = () => {
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
-          <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+          <p>{isSignup ? "Register" : "Sign In"}</p>
           <form onSubmit={handleSubmit}>
             {isSignup && (
               <div className="auth__form-container_fields-content_input">
                 <label htmlFor="fullName">Full Name</label>
                 <input
+                  autoComplete="off"
                   name="fullName"
                   type="text"
                   placeholder="Full Name"
@@ -52,6 +88,7 @@ const Auth = () => {
             <div className="auth__form-container_fields-content_input">
               <label htmlFor="username">Username</label>
               <input
+                autoComplete="off"
                 name="username"
                 type="text"
                 placeholder="Username"
@@ -63,6 +100,7 @@ const Auth = () => {
               <div className="auth__form-container_fields-content_input">
                 <label htmlFor="phoneNumber">Phone Number</label>
                 <input
+                  autoComplete="off"
                   name="phoneNumber"
                   type="text"
                   placeholder="Phone Number"
@@ -75,6 +113,7 @@ const Auth = () => {
               <div className="auth__form-container_fields-content_input">
                 <label htmlFor="avatarURL">Avatar URL</label>
                 <input
+                  autoComplete="off"
                   name="avatarURL"
                   type="text"
                   placeholder="Avatar URL"
@@ -86,6 +125,7 @@ const Auth = () => {
             <div className="auth__form-container_fields-content_input">
               <label htmlFor="password">Password</label>
               <input
+                autoComplete="off"
                 name="password"
                 type="password"
                 placeholder="Password"
@@ -97,6 +137,7 @@ const Auth = () => {
               <div className="auth__form-container_fields-content_input">
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
+                  autoComplete="off"
                   name="confirmPassword"
                   type="password"
                   placeholder="Confirm Password"
@@ -111,16 +152,68 @@ const Auth = () => {
           </form>
           <div className="auth__form-container_fields-account">
             <p>
-              {isSignup ? "Already have an account?" : "Don't have an account?"}
+              {isSignup
+                ? "Already have an account? "
+                : "Don't have an account? "}
               <span onClick={switchMode}>
-                {isSignup ? "Sign in" : "Sign Up"}
+                {isSignup ? "Sign In" : "Register"}
               </span>
             </p>
           </div>
         </div>
       </div>
+      <ToastContainer autoClose={10000} />
+
       <div className="auth__form-container_image">
-        <img src={signinImage} />
+        <Particles
+          id="tsparticles"
+          options={{
+            background: {
+              color: {
+                value: "rgb(37, 25, 12)",
+              },
+            },
+            fpsLimit: 60,
+
+            particles: {
+              color: {
+                value: "#fff",
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 1080,
+                },
+                limit: 0,
+                value: 400,
+              },
+              opacity: {
+                animation: {
+                  enable: true,
+                  minimumValue: 0.05,
+                  speed: 3,
+                  sync: false,
+                },
+                random: {
+                  enable: true,
+                  minimumValue: 0.05,
+                },
+                value: 1,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: {
+                  enable: true,
+                  minimumValue: 0.5,
+                },
+                value: 1,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
       </div>
     </div>
   );
--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
 
-const TeamChannelPreview = ({ channel, type }) => {
+const TeamChannelPreview = ({
+  setActiveChannel,
+  setIsCreating,
+  setIsEditing,
+  setToggleContainer,
+  channel,
+  type,
+}) => {
   const { channel: activeChannel, client } = useChatContext();
 
   const ChannelPreview = () => (
@@ -14,14 +21,15 @@ const TeamChannelPreview = ({ channel, type }) => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id !== client.userID,
     );
+
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.fullName || members[0]?.user?.id}
           size={24}
         />
-        <p>{members[0]?.user?.fullName}</p>
+        <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
       </div>
     );
   };
@@ -34,7 +42,12 @@ const TeamChannelPreview = ({ channel, type }) => {
           : "channel-preview__wrapper"
       }
       onClick={() => {
-        console.log(channel);
+        setIsCreating(false);
+        setIsEditing(false);
+        setActiveChannel(channel);
+        if (setToggleContainer) {
+          setToggleContainer(prevState => !prevState);
+        }
       }}
     >
       {type === "team" ? <ChannelPreview /> : <DirectPreview />}
